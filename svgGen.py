d16a494907933753b46c94faf1d288712d97b0db
smdennis8 <50685398+smdennis8@users.noreply.github.com>
2021-01-11 21:31:06 -0500

    Add files via upload

---
+++ svgGen.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def general_funct(args1, args2):
+    pass
+
+
+def main():
+    "put main runing code here"
+    
+    pass
+
+    
+if __name__ == "__main__":
+    main()
